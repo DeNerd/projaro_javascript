4ed4b41aac4df15b2dcb5a22a45a0672385d3811
Umeh Emeka Edmund <DeNerd@De-Nerds-MacBook-Pro.local>
2018-03-16 18:37:41 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,132 @@
+// // var firstName = prompt("What is your first name?");
+// // var lastName = prompt("Your last name here, please");
+// // var age = prompt("How old are you?");
+
+// // console.log("Your full name is " + firstName + " " + lastName);
+// // console.log("You are " + age + " " + "years old.");
+
+// // var age = prompt("How old are you?");
+// // var ageInDays = age * 365.25;
+// // alert("You are " + " roughly " + ageInDays + " days" + " old.");
+// // console.log("You " + "are " + ageInDays + " days" + " old." + " Waoh!")
+
+// // Print all numbers between -10 and 19
+// Solution using while loop
+// -------------------------
+// // var num = -10;
+// // while(num <= 19) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+Solution using for loop
+-----------------------
+
+for(var i = -10; i <= 19; i++) {
+    console.log(i);
+}
+
+// // Print all even numbers between 10 and 40
+Solution using while loop
+-------------------------
+// var num = 10;
+// while(num <= 40) {
+//     console.log(num);
+//     num+=2;
+// }
+
+Solution using for loop
+-----------------------
+
+for(var i = 10; i <= 40; i+=2) {
+    console.log(i);
+}
+
+// // Print all odd numbers between 300 and 333
+Solution using while loop
+-------------------------
+//  var num = 300;
+//  while(num <= 333) {
+//     if (num % 2 !== 0) {
+//      console.log(num);
+//     }
+//      num+=1;
+//  }
+
+Solution using for loop
+-----------------------
+
+for(var i = 300; i <= 333; i++) {
+    if(i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+// // Print all numbers divisible by 5 AND 3 betweeen 5 and 50
+
+Solution using while loops
+--------------------------
+
+// var num = 5;
+// while(num <= 50) {
+//     if(num % 5 === 0 && num % 3 === 0);{
+//     console.log(num);
+//  }
+//     num=+1;
+// }
+
+Solution using for loops
+------------------------
+
+for(var i = 5; i <= 50; i++) {
+    if(i % 5 === 0 && i % 3 === 0) {
+        console.log(i);
+    }
+}
+
+
+// var answer = prompt("Are we there yet?");
+
+// while(answer !== "yes" && answer !== "yeah") {
+//     var answer = prompt("Are we there yet?");
+// }
+
+// alert("YAY, we made it!");
+
+// var str = "EmekaGift";
+// var count = 0;
+// while (count < str.length) {
+//     console.log(str[G]);
+//     count++;
+// }
+
+// for(var i = 0; i < 16; i+=8) {
+//     console.log(i);
+// }
+
+// var str = "ahceclwlxo";
+// for(var i = 1; i < str.length; i+=2) {
+//     console.log(str[i]);
+// }
+
+// var x = 5;
+
+// var y = x;
+
+// x = 4;
+
+// console.log(y);
+
+// function myFunc(name) {
+//     console.log("welcome, " + name)
+// }
+// myFunc();
+
+// function yourName(Emeka) {
+//     alert("What is your name?")
+// }
+
+// function welcome(Emeka) {
+//     console.log("Welcome " + Emeka);
+
+// }
